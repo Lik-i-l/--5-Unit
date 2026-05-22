0659fe42312500929bc581ab283949b59fe1fb0d
Lik-i-l <nik28092000@yandex.ru>
2026-05-22 11:21:05 +0300

    Delete lab3 directory

--- lab3/math.test.ts
+++
@@ -1,9 +0,0 @@
-import { f } from './math';
-
-const b = 2.5;
-
-test('проверка 3.9', () => {
-    const result = f(3.9, b);
-    console.log(`Результат f(3.9, 2.5) = ${result}`);
-    expect(result).toBeCloseTo(0.14, 2);
-});
