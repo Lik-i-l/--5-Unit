8f697b4ef7b70972ec84de9f75250f3ec3e18e66
Lik-i-l <nik28092000@yandex.ru>
2026-05-22 11:08:16 +0300

    Update math.test.ts

--- lab3/math.test.ts
+++ lab3/math.test.ts
@@ -1,37 +1,35 @@
 import { f } from './math';
 
-describe("Функция f(x, b)", () => {
-    const b: number = 2.5;
+const b = 2.5;
 
-    it("x = 1.28", () => {
-        expect(f(1.28, b)).toBeCloseTo(0.26, 2);
-    });
+test('x = 1.28', () => {
+    expect(f(1.28, b)).toBeCloseTo(0.26, 2);
+});
 
-    it("x = 3.28", () => {
-        expect(f(3.28, b)).toBeCloseTo(0.16, 2);
-    });
+test('x = 3.28', () => {
+    expect(f(3.28, b)).toBeCloseTo(0.16, 2);
+});
 
-    it("x = 0.4", () => {
-        expect(f(0.4, b)).toBeCloseTo(0.87, 2);
-    });
+test('x = 0.4', () => {
+    expect(f(0.4, b)).toBeCloseTo(0.87, 2);
+});
 
-    it("x = 1.1", () => {
-        expect(f(1.1, b)).toBeCloseTo(0.37, 2);
-    });
+test('x = 1.1', () => {
+    expect(f(1.1, b)).toBeCloseTo(0.37, 2);
+});
 
-    it("x = 2.4", () => {
-        expect(f(2.4, b)).toBeCloseTo(0.20, 2);
-    });
+test('x = 2.4', () => {
+    expect(f(2.4, b)).toBeCloseTo(0.20, 2);
+});
 
-    it("x = 3.6", () => {
-        expect(f(3.6, b)).toBeCloseTo(0.15, 2);
-    });
+test('x = 3.6', () => {
+    expect(f(3.6, b)).toBeCloseTo(0.15, 2);
+});
 
-    it("x = 1.7", () => {
-        expect(f(1.7, b)).toBeCloseTo(0.29, 2);
-    });
+test('x = 1.7', () => {
+    expect(f(1.7, b)).toBeCloseTo(0.29, 2);
+});
 
-    it("x = 3.9", () => {
-        expect(f(3.9, b)).toBeCloseTo(0.14, 2);
-    });
+test('x = 3.9', () => {
+    expect(f(3.9, b)).toBeCloseTo(0.14, 2);
 });
